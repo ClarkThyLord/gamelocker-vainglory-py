1850511856eafd94f509fa3311ec7c3afdd1fe55
ClarkThyLord <clarck71@outlook.com>
2017-04-22 12:47:17 -0700

    fix?

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.txt", "r") as txt:
 setup(
 
     name="madglory-ezl",
-    version="0.5.0",
+    version="0.5.1",
     author="ClarkthyLord",
     author_email="clark71@outlook.com",
     url="https://github.com/ClarkThyLord/madglory-ezl",
     keywords="madglory python 3.5 vainglory",
     description="A python wrapper for MadlGlory's gamelocker.",
     long_description=long_description,
-    package="madglory-ezl",
+    package=["madglory-ezl"],
     install_requires=[
 
         "requests"
