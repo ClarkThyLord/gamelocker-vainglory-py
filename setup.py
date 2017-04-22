457a1e174c80b054a4485a755ab39abdcb408915
ClarkThyLord <clarck71@outlook.com>
2017-04-22 11:45:13 -0700

    added setup
    clark was here :v

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.txt", "r") as txt:
+    long_description = txt.read()
+
+setup(
+
+    name="madglory-ezl",
+    version="0.5.0",
+    author="ClarkthyLord",
+    author_email="clark71@outlook.com",
+    url="https://github.com/ClarkThyLord/madglory-ezl",
+    keywords="madglory python 3.5 vainglory",
+    description="A python wrapper for MadlGlory's gamelocker.",
+    long_description=long_description,
+    package="madglory-ezl",
+    install_requires=[
+
+        "requests"
+
+    ]
+
+)
