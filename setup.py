4bebeee19ce894222f0c65f2a1ad8a258afbcf22
ClarkThyLord <clarck71@outlook.com>
2017-04-23 18:40:49 -0700

    fix?

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.txt", "r") as txt:
 setup(
 
     name="gamelocker",
-    version="0.5.2",
+    version="0.5.3",
     author="ClarkthyLord",
     author_email="clark71@outlook.com",
     url="https://github.com/ClarkThyLord/gamelocker",
-    description="A python wrapper for MadlGlory's gamelocker.",
+    description="A python3.5 wrapper for MadlGlory's gamelocker.",
     long_description=long_description,
     keywords="madglory python 3.5 vainglory",
     packages=["gamelocker"],
